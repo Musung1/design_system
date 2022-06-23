b6a6233dbea4e581fbf41ef0aaeb79d78a4f5010
Kim Musung <musungkim1352@gmail.com>
2022-06-23 09:41:33 +0900

    add controller

--- lib/home.dart
+++ lib/home.dart
@@ -2,13 +2,16 @@ import 'package:design_project/design_system/theme/theme/app_buttonstyle.dart';
 import'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'controller/home_controller.dart';
 import 'design_system/theme/theme/app_color.dart';
 import 'design_system/theme/theme/app_theme.dart';
+import 'design_system/widget/chips/mychip.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Get.put(HomeController());
     return Scaffold(
       appBar : AppBar(
         title : Text("hi"),
@@ -31,19 +34,21 @@ class Home extends StatelessWidget {
               child: Text("textButton"),
             ),
             ElevatedButton(
-              onPressed: (){},
+              onPressed: (){
+              },
               style: AppButtonStyle.myButtonStyle,
               child:Text("button"),
             ),
-            Container(
-              height: 200,
-              width : 200,
-            ),
+            const MyChip(label: "myChip",function: function),
           ],
         ),
       ),
     );
   }
 }
+void Function()? function(){
+  print("hi");
+}
+
 
     
